b90e5c6ca4d16f87d5f550c246fda401e9b2e9a6
Dawid Draguła <dawid.dragula@gmail.com>
2024-04-09 11:43:53 +0200

    Added script to turn on all the markers.

---
+++ js/sync-demo-utils.js
@@ -0,0 +1,12 @@
+console.clear();
+
+Highcharts.setOptions({
+    plotOptions: {
+        series: {
+            marker: {
+                enabled: true,
+                radius: 1
+            }
+        }
+    }
+});
